ecb716ca0936a5497b79230b891f994eac1be259
drivet <drivet@akio.com>
2025-11-06 19:24:30 +0100

    first commit

---
+++ tests/user.first.spec.ts
@@ -0,0 +1,8 @@
+// import { test, expect } from '@playwright/test';
+
+// test.describe('a first test on user', () => {
+//   test('verify presence on info utiles on home page', async({page}) => {
+//     await page.goto('local.machine')
+//     await expect(page).toHaveTitle('coucou')
+// } )  
+// });
